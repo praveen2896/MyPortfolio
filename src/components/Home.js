40d30fd0ac82d6c51adcc1d52a92d4221fe013e0
praveen2896 <mpraveenkumarown@gmail.com>
2019-05-27 23:11:52 +0530

    changing profile pic

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,13 @@
-import React from "react";
-import Age from "../components/birth";
-import Image5 from "../images/HTML2.png";
-import Image3 from "../images/JavaImg.png";
-import Image7 from "../images/javascript.png";
-import Image2 from "../images/node1.png";
-import Image1 from "../images/ReactImg.png";
-import Image4 from "../images/spring_bot.png";
-import "./Home.css";
+import React from "react"
+import Age from "../components/birth"
+import Image5 from "../images/HTML2.png"
+import Image3 from "../images/JavaImg.png"
+import Image7 from "../images/javascript.png"
+import Image2 from "../images/node1.png"
+import Image1 from "../images/ReactImg.png"
+import Image4 from "../images/spring_bot.png"
+import Image6 from "../images/profile.jpg"
+import "./Home.css"
 
 const bigAvatar = {
   // margin: 10,
@@ -20,13 +21,7 @@ const Home = () => {
     <div>
       <div className="skillsavatar">
         <div container justify="center" alignItems="center">
-          <img
-            src={
-              "https://images.unsplash.com/photo-1555139356-4c9b9f3f28a6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=282&q=80"
-            }
-            alt="Avatar"
-            className="bigavatar"
-          />
+          <img src={Image6} alt="Avatar" className="bigavatar" />
         </div>
         <div container justify="center" alignItems="center" className="name">
           <strong>
